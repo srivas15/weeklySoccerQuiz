ca265d64a5ba50b7b2aa7043f9c307437a1debd9
Sharoon Srivastava <srivas15@purdue.edu>
2017-09-20 23:27:14 -0700

    start page design

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
+
+@app.route('/test')
+def test():
+	print 'WOW'
