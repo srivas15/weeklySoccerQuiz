804bb3770089c5c7067afb68cbe07f524ac65913
Sharoon Srivastava <srivas15@purdue.edu>
2017-09-30 13:35:45 -0700

    testing for AWS

--- application.py
+++ application.py
@@ -1,3 +1,28 @@
 #!flask/bin/python
 from app import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return ('WOW')
+# print a nice greeting.
+
+# EB looks for an 'application' callable by default.
+# application = Flask(__name__)
+
+# add a rule for the index page.
+# app.add_url_rule('/', 'index', ("ok"))
+
+# add a rule when the page is accessed with a name appended to the site
+# URL.
+# application.add_url_rule('/<username>', 'hello', (lambda username:
+    # header_text + say_hello(username) + home_link + footer_text))
+
+# run the app.
+# if __name__ == "__main__":
+#     # Setting debug to True enables debug output. This line should be
+#     # removed before deploying a production app.
+#     application.debug = True
+#     application.run()
 app.run(debug=True)
