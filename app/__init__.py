ddaba1135fd388dbb615fce99f46cde65e5650f6
Sharoon Srivastava <srivas15@purdue.edu>
2017-09-30 15:24:21 -0700

    working except static files trying new path

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# from flask.ext.sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 app.config.from_object('config')
--- application.py
+++ application.py
@@ -3,11 +3,6 @@ from app import app
 # EB looks for an 'application' callable by default.
 application = app
 
-# @app.route('/')
-# @app.route('/index')
-# def home():
-#     return 'WOW'
-
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
