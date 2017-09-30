35e8d60c89edb323a82108059e5c78e382980ba9
Sharoon Srivastava <srivas15@purdue.edu>
2017-09-30 12:59:03 -0700

    changes to models and config to deploy on AWS

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from app import db
 
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     social_id = db.Column(db.String(64), nullable=False, unique=True)
     userName = db.Column(db.String(64), index=True, unique=True)
@@ -9,12 +9,12 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User %r>' % (self.email)
 
-class Points(db.Model):
-	id = db.Column(db.Integer, primary_key=True, db.ForeignKey(User.id))
-	points = db.Column(db.Integer)
+# class Points(db.Model):
+# 	id = db.Column(db.Integer, primary_key=True, db.ForeignKey(User.id))
+# 	points = db.Column(db.Integer)
 
-class GWinfo():
-	id = db.Column(db.Integer, primary_key=True, db.ForeignKey(User.id))
-	playedThisGW = db.Column(db.Boolean)
-	lastQuestionAnswered = db.Column(db.Integer)
-	UpdateDtTime = db.Column(db.DateTime)
+# class GWinfo():
+# 	id = db.Column(db.Integer, primary_key=True, db.ForeignKey(User.id))
+# 	playedThisGW = db.Column(db.Boolean)
+# 	lastQuestionAnswered = db.Column(db.Integer)
+# 	UpdateDtTime = db.Column(db.DateTime)
--- config.py
+++ config.py
@@ -1,8 +1,22 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+# import os
+# basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+# edit the URI below to add your RDS password and your AWS URL
+# The other elements are the same as used in the tutorial
+# format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sharoon61295:goodmorning15@weeklysoccerquiz.cj7ws5wt0yje.us-east-2.rds.amazonaws.com:3306/weeklySoccerQuiz'
+
+# Uncomment the line below if you want to work with a local DB
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+
+SQLALCHEMY_POOL_RECYCLE = 3600
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
 
 questions = [	
 	"Who scored a hat-trick last gameweek?",
